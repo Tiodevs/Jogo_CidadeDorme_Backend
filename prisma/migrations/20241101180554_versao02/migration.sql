bc863b22831f1cc573e7ee69e57131413c8b8cff
Tiodevs <Tiodevs>
2024-11-01 15:09:50 -0300

    aaaaa

---
+++ prisma/migrations/20241101180554_versao02/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Players" ALTER COLUMN "life" SET DEFAULT 2;
+
+-- AlterTable
+ALTER TABLE "Room" ALTER COLUMN "Day" SET DEFAULT 1;
--- src/services/logics/EndVotes.ts
+++ src/services/logics/EndVotes.ts
@@ -2,7 +2,6 @@ import prismaClient from "../../prisma";
 
 class EndVotes {
   async execute(room_id: string) {
-
     const room = await prismaClient.room.findFirst({
       where: { id: room_id }
     });
@@ -10,33 +9,41 @@ class EndVotes {
     if (!room) throw new Error("Sala não encontrada.");
 
     const votes = await prismaClient.votes.findMany({
-      where:{
+      where: {
         day: room.Day,
         roomId: room_id
       },
-      include:{
+      include: {
         player: true
       }
-    })
+    });
 
     const voteCount: { [key: string]: number } = {};
 
+    // Contagem dos votos para cada jogador
     votes.forEach((vote) => {
       const playerId = vote.playerId;
       voteCount[playerId] = (voteCount[playerId] || 0) + 1;
     });
 
+    // Encontrar o número máximo de votos
     let maxVotes = 0;
-    let playerIdWithMostVotes = null;
-
-    for (const playerId in voteCount) {
-      if (voteCount[playerId] > maxVotes) {
-        maxVotes = voteCount[playerId];
-        playerIdWithMostVotes = playerId;
+    for (const count of Object.values(voteCount)) {
+      if (count > maxVotes) {
+        maxVotes = count;
       }
     }
 
-    if (playerIdWithMostVotes) {
+    // Encontrar todos os jogadores com o número máximo de votos
+    const playersWithMaxVotes = Object.keys(voteCount).filter(
+      playerId => voteCount[playerId] === maxVotes
+    );
+
+    // Se houver apenas um jogador com o número máximo de votos, eliminá-lo
+    let playerIdWithMostVotes = null;
+
+    if (playersWithMaxVotes.length === 1) {
+      playerIdWithMostVotes = playersWithMaxVotes[0];
       await prismaClient.players.update({
         where: { id: playerIdWithMostVotes },
         data: {
@@ -44,12 +51,28 @@ class EndVotes {
           Active: false
         }
       });
+      console.log("Player com mais votos", playerIdWithMostVotes);
+    } else {
+      console.log("Empate: Nenhum jogador será eliminado.");
     }
 
-    // Retornar informações sobre o jogador que foi "eliminado"
+    // Avançar o dia no jogo
+    const newDay = room.Day + 1;
+
+    await prismaClient.room.update({
+      where: {
+        id: room_id
+      },
+      data: {
+        Day: newDay
+      }
+    });
+
+    // Retornar informações sobre o jogador eliminado (ou nulo se houver empate)
     return {
       playerId: playerIdWithMostVotes,
-      votes: maxVotes
+      votes: maxVotes,
+      tie: playersWithMaxVotes.length > 1 // Indica se houve empate
     };
   }
 }
